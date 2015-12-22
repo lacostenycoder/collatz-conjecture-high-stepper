9284729e7a6b4d0b5accb97f076c0ab5d967115b
lacostenycoder <djlancejordan@gmail.com>
2015-12-22 14:48:57 -0500

    us oj gem for faster json parsing. cleanup json read write logic

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Collatz Conjecture High Steps Solver
 
 A simple ruby program using the Collatz Conjecture to find the number with greatest number of steps to reach zero.
-Includes explaination and timer as calculations grow with big numbers.  Solves up to 1,000,000 in 10 seconds or less.
- 
+Includes explaination and timer as calculations grow with big numbers.  Solves up to 1,000,000 in 10 seconds or less.  
+
+In order to improve performance on large numbers, key value pairs are now stored in a local json file created/updated dynamically if the input key is not already in file.  This might be seen as cheating, but for now I have not found a faster solution.  The original approach of block passed to hash worked well. I moved the block into a lambda to return value where item already exists in the hash.
+
 This was asked of me on a code test where the developer testing me suggested I used recursion but I found recursion for
 this problem to blow up my computer.  Please let me know if you find a better solution or a way where recursion works.
 
 =======
 
-##### Cross reference step checking here: 
+##### Cross reference step checking here:
 [http://skanderkort.com/collatz_conjecture_calculator](http://skanderkort.com/collatz_conjecture_calculator)
--- collatz.rb
+++ collatz.rb
@@ -1,19 +1,44 @@
+#require 'json'
+require 'oj'
+require 'fileutils'
+
 puts "Welcome to Lance Jordan solution to Collatz Conjecture"
 puts "What is the Collatz Conjecture?"
 puts "\n\r Take any natural number n. If n is even, divide it by 2 to get n / 2. If n is odd, multiply it by 3 and add 1 to obtain 3n + 1. Repeat the process (which has been called 'Half Or Triple Plus One', or HOTPO[6]) indefinitely. The conjecture is that no matter what number you start with, you will always eventually reach 1."
 puts "\n\r This code will solve the highest number of steps given an integer and includes a timer function."
-
+puts "\n\r \n\r\ Larger numbers will save to json file for faster loading later."
 puts "Enter a positive integer below 1000000 : "
 
 iterate = gets.chomp.to_i
-
+input = iterate.to_s.to_i # create new obj so value doesn't change
 start_time = Time.now
+json_open = false
 
-hash = Hash.new
+def json_empty?(string)
+	string == "{}" || string == '' || string == nil ? true : false
+end
 
-collatz = lambda do |key|
-	return hash[key] if hash[key]
-	h = hash
+# don't use json on smaller numbers
+if input > 99999
+	FileUtils.touch 'lookup.json' # insure file exsists before writing
+	jsonfile = File.read('lookup.json')
+	json_open = true
+end
+
+# only load json file if data exsists
+if json_open && !json_empty?(jsonfile)
+	#hash = JSON.parse(jsonfile)
+	hash = Oj.load(jsonfile)
+	puts "json loaded"
+else
+	hash = Hash.new
+end
+
+@hash_limit = hash.select{|k,v| k.to_i <= input} || Hash.new
+
+collatz = lambda do |key, this_hash|
+	return this_hash[key] if this_hash[key]
+	h = this_hash
 	k = key
 	if k < 2
 		h[k] = k
@@ -27,25 +52,40 @@ collatz = lambda do |key|
 				num = num * 3 + 1
 			end
 			count += 1
-			if hash[num]
-				return hash[key] = hash[num] + count
+			if this_hash[num]
+				return this_hash[key] = this_hash[num] + count
 			end
 		end
 		h[k] = count
 	end
 end
 
-def largest_hash_key(hash)
-	hash.max_by{|k,v| v}
+def largest_hash_key
+	@hash_limit.max_by{|k,v| v}
 end
 
-while iterate > 2
-	collatz.call iterate
-	iterate -= 1
+def largest_in_json
+	@hash_limit.sort_by{|k,v| v}.last
 end
 
-key = largest_hash_key(hash)[0]
+if hash[input.to_s]
+	key = largest_in_json[0]
+else
+	while iterate > 2
+		collatz.call iterate, @hash_limit
+		iterate -= 1
+	end
+	key = largest_hash_key[0]
+end
 
-puts "number with most steps = " + largest_hash_key(hash)[0].to_s
-puts "it takes #{hash[key].to_s} steps to reach zero."
+if json_open && !hash[input]
+	File.open("lookup.json","w") do |f|
+		f.write Oj.dump(@hash_limit)
+		#f.write(hash.to_json)
+	end
+	puts "json saved"
+end
+
+puts "number with most steps = " + key.to_s
+puts "it takes #{@hash_limit[key].to_s} steps to reach zero."
 puts "completed in :" + (Time.now - start_time).to_s + " seconds"
